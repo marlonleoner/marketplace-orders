4b330e226a50a1abe2735c55e879d8ac515bd0c0
Marlon Leoner <marlon.leoner@gmail.com>
2021-02-11 11:50:02 -0300

    swagger

--- src/main/java/br/com/ufsm/order/api/security/WebSecurityConfig.java
+++ src/main/java/br/com/ufsm/order/api/security/WebSecurityConfig.java
@@ -3,10 +3,10 @@ package br.com.ufsm.order.api.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -44,14 +44,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
 		httpSecurity.authorizeRequests()
-				// Auth
-				.antMatchers(HttpMethod.POST, "/auth").permitAll()
-				// Products
-				.antMatchers(HttpMethod.POST, "/usuarios").permitAll()
-				// Others requests
+				// All Requests
 				.anyRequest().authenticated().and().csrf().disable().sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 				.addFilterBefore(authRequestFilter, UsernamePasswordAuthenticationFilter.class);
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**",
+				"/configuration/security", "/swagger-ui.html", "/webjars/**");
 	}
 
 }
